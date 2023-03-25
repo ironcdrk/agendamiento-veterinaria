5aa0ef3a9d4afff8a317bb63bf12181aa7d1b8aa
Carlos Licto <carlosdarkslayer@gmail.com>
2023-03-24 22:55:20 -0500

    Se arreglan los estilos, se añade boton login, se termina de cuadrar el map del esqueleto del perro

--- js/atencion.js
+++ js/atencion.js
@@ -5,10 +5,26 @@ import { CanvasMapHighlight } from './maphighlight.js';
 const maphighlight = new CanvasMapHighlight({'canvasMap' : 'regionsMapCanvas'});
 const mapAreas = document.querySelectorAll(".area");
 
+// maphighlight.buildHighlightArea([300,0,200,200]); //craneo
+// maphighlight.buildHighlightArea([80,150,250,70]); //lumbar
+// maphighlight.buildHighlightArea([10,180,80,155]); // cola
+// maphighlight.buildHighlightArea([10,320,160,160]); // extremidades posteriores
+// maphighlight.buildHighlightArea([200,320,160,190]); // extremidades delanteras
+// maphighlight.buildHighlightArea([80,210,250,70]); // abdominal
+
+
 mapAreas.forEach((element)=>{
-    element.addEventListener('mouseover', function(ev){
-        const coordsArray=element.coords.split(",").map((item)=>parseInt(item));
-        console.log(coordsArray);
-        maphighlight.buildHighlightArea(coordsArray);
+    element.addEventListener('mouseover', function(event){
+        const coordsArray=element.dataset.coords.split(",").map((item)=>parseInt(item));
+        // console.log(coordsArray);
+        maphighlight.build(coordsArray);
+    });
+    element.addEventListener('mouseout', function(event){
+        // console.log(coordsArray);
+        maphighlight.destroy();
+    });
+    element.addEventListener('click', function(event){
+        console.log(element.title);
+        
     });
 });
--- js/maphighlight.js
+++ js/maphighlight.js
@@ -1,25 +1,30 @@
 export class CanvasMapHighlight{
     constructor(options){
-        console.log("options "+options);
-        console.log("canvasMap "+options.canvasMap);
+        
         this.canvasMap=document.getElementById(options.canvasMap);
-        //this.coords= option.coords;
-        this.bgcolor = options.bgcolor || 'rgba(0,0,0,0.2)';
-        this.ctx = this.canvasMap.getContext("2d");        
-        this.ctx.drawImage(document.getElementById("scream"),10,10);
+        this.bgcolor = options.bgcolor || 'rgba(0,0,0,0.3)';     
     }
 
-    buildHighlightArea(coords) {
-        console.log("ctx "+this.ctx);
-        // TODO validar existencia de canvas
-        // ctx.font = "30px Arial";
-        // ctx.strokeText("Hello World", 10, 50);
+    build(coords) {
+        try{
+            this.ctx = this.canvasMap.getContext("2d");   
+        }
+        catch(error){
+            console.error(e); 
+        }
         //this.ctx.clearRect(0, 0, this.canvasMap.width,this.canvasMap.height);
         this.ctx.fillStyle = this.bgcolor;
-        console.log('coords');
-        console.log(...coords);
         this.ctx.fillRect(...coords);
-        this.canvasMap.setAttribute('style','opacity: 0');
+    }
+
+    destroy(coords) {
+        try{
+            this.ctx = this.canvasMap.getContext("2d");   
+        }
+        catch(error){
+            console.error(e); 
+        }
+        this.ctx.clearRect(0, 0, this.canvasMap.width,this.canvasMap.height);
     }
         
 }
