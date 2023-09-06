da7e23cce93d0f788c1edc2680e9017276c96b5f
Carlos Licto <carlosdarkslayer@gmail.com>
2023-09-05 20:09:12 -0500

    cambio de tìtulo de cada secciòn y de referencia a archivo css hour picker

--- js/calendar.js
+++ js/calendar.js
@@ -1,20 +1,53 @@
-// const daysofweek = ['L','M','M','J','V','S','D'];
-// class Calendar{
-//     constructor(options){
-//         this.date=options.date || new Date();
-//         this.container=options.container;
+const daysofWeek = ['L','M','M','J','V','S','D'];
+const mapdaysofMonth= [];
+export class Calendar{
+     constructor(options){
+        console.log(options);
+        this.date=options.date || new Date();
+         this.container=options.container;
+         this.buildCalendar();
+         this.updateCalendar();
 
-//         this.buildCalendar();
+     }
 
-//     }
+    buildCalendar(){
+         const containerHtml=document.querySelector(this.container);
+        //  daysofWeek.forEach((item,i)=>{
+        //      let dayName = document.createElement('div');
+        //      dayName.classList.add('calendar_number');
+        //      dayName.innerHTML=item;
+        //      containerHtml.appendChild(dayName);
+        //  });
+     }
 
-//     buildCalendar(){
-//         const containerHtml=document.querySelector(this.container);
-//         daysofweek.forEach((item,i)=>{
-//             let dayName = document.createElement('div');
-//             dayName.classList.add('calendar_day');
-//             dayName.innerHTML=item;
-//             containerHtml.appendChild(dayName);
-//         });
-//     }
-// }
+    calculateDates(){
+        this.monthStart = new Date(this.date.getFullYear(), this.date.getMonth(),1);
+        this.monthEnd = new Date(this.date.getFullYear(), this.date.getMonth()+1,1);
+        this.monthLength = Math.floor((this.monthEnd - this.monthStart)/(1000*60*60*24));
+    }
+
+    updateCalendar(){
+        this.calculateDates();
+
+        let firstDayInWeek = this.monthStart.getDay();
+        console.log("firstDayInWeek: "+firstDayInWeek);
+        const containerHtml=document.querySelector(this.container);
+        let currentDay=1;
+        for(let row=1; row<42; row++){
+            let dayName = document.createElement('div');
+            dayName.innerHTML="";
+            if(row>=firstDayInWeek){
+                dayName.classList.add('calendar_number');
+                dayName.innerHTML=currentDay;
+                currentDay++;
+            }
+            containerHtml.appendChild(dayName);
+            if(currentDay>this.monthLength){
+                break;
+            }
+        }
+        //console.log(daysofWeek[firstDayInWeek]);
+
+    }
+
+}
